59a263ec32a91b538935b1f96cc8b3f6023b2c53
Alvaro Ortiz Pedroso <alvaro.ortiz.pedroso@gmail.com>
2023-03-09 09:31:07 +0100

    Se anhade navbar y componente granja

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GranjaComponent } from './granja/granja.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GranjaComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/granja/granja.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-granja',
+  templateUrl: './granja.component.html',
+  styleUrls: ['./granja.component.css']
+})
+export class GranjaComponent {
+
+}
